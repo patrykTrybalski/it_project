f15cc1a3596ada0bce1463cbf504b492e9ddf66a
patrykTrybalski <trybalski.patryk@gmail.com>
2019-10-20 11:43:19 +0200

    connected with db

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,46 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const {Todo} = require('./models/todo');
+const {URL, DB_NAME} = require('./config/DB');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+mongoose.connect(`${URL}${DB_NAME}`, {useNewUrlParser: true});
+mongoose.connection.on('error', () => console.error.bind(console, 'connection error:'));
+mongoose.connection.once('open', () => console.log('connected'));
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+});
+
+app.post('/todo', (req, res) => {
+    Promise.resolve()
+        .then(() => new Todo(req.body))
+        .then(todo => todo.save())
+        .then(todo => res.send(todo))
+        .catch(err => res.send(err));
+});
+
+app.get('/todo', (req, res) => {
+    Promise.resolve()
+        .then(() => Todo.find({}))
+        .then(todo => res.send(todo))
+        .catch(err => res.send(err));
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        'errors': {
+            message: err.message,
+            error: err
+        }
+    });
+});
+
+const server = app.listen(3000, () => {
+    console.log('Listening on port ' + server.address().port);
+});
